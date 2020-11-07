ba1e749f8c1a5415e5b2878f987a2922ec20e942
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-11-08 05:27:48 +0530

    khjgv

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          <li key="relativeListItem1">Shaik</li>
+          <li key="relativeListItem2">Mohammed</li>
+          <li key="relativeListItem3">Zubaidi</li>
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
